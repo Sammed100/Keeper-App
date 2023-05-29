42d65623cb05d60ba3f031986edb6e31df6bbc33
Sammed100 <sammedjain100@gmail.com>
2023-05-29 15:31:33 +0530

    Updates

---
+++ precache-manifest.c3b53a55cb1615dc977332c4b9eeb678.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64edc06e54ecf3f9fc445fb1205cc3e4",
+    "url": "/Keeper-App/index.html"
+  },
+  {
+    "revision": "140303718ebb369fd3fc",
+    "url": "/Keeper-App/static/js/2.e928da4b.chunk.js"
+  },
+  {
+    "revision": "9cc91b293eeb207e8cb1",
+    "url": "/Keeper-App/static/js/main.1c48aba0.chunk.js"
+  },
+  {
+    "revision": "53168b6972fe4da5e554",
+    "url": "/Keeper-App/static/js/runtime-main.5f8222f9.js"
+  }
+]);
